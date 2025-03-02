480f720da9b2aef19add7e96b7429e88a3b221ef
Enocha Engulu <enochaengulu@gmail.com>
2025-03-02 22:37:41 +0300

    Add Vercel configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000, // Set a specific port for consistency
-    open: true  // Automatically open the browser
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
-}) 
+})
